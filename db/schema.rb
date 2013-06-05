8ed2cf104837bc8127b675a55c21a7a6781585c3
Abeer Minhas <aminhas@Abeers-MacBook-Pro.local>
2013-06-05 15:13:28 -0400

    Added proper migration table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130605173914) do
+ActiveRecord::Schema.define(:version => 20130605191139) do
 
   create_table "menu_items", :force => true do |t|
     t.string   "name"
@@ -38,8 +38,10 @@ ActiveRecord::Schema.define(:version => 20130605173914) do
   create_table "reviews", :force => true do |t|
     t.string   "comment"
     t.integer  "rating"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at",    :null => false
+    t.datetime "updated_at",    :null => false
+    t.integer  "restaurant_id"
+    t.string   "name"
   end
 
 end
